4131d64f408aca34da934bf9e50effb58bf76f1d
Koblandy <kbaizhigit@outlook.com>
2019-09-15 23:01:08 +0600

    Added logging inteceptor

--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -1,13 +1,19 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
 
 import { UserService } from './user.service';
-
 import { UserSchema } from '../models/user.schema';
+import { HttpExceptionFilter } from './http-exception.filter';
+import { LoggingInterceptor } from './logging.interceptor';
 
 @Module({
   imports: [MongooseModule.forFeature([{ name: 'User', schema: UserSchema }])],
-  providers: [UserService],
-  exports: [UserService]
+  providers: [
+    UserService,
+    { provide: APP_FILTER, useClass: HttpExceptionFilter },
+    { provide: APP_INTERCEPTOR, useClass: LoggingInterceptor },
+  ],
+  exports: [UserService],
 })
 export class SharedModule {}
